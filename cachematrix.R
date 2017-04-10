83b3f47b188c5d134307d10e59b8ed5af86d27b2
eslam-ghazal <eslamm.gazal@gmail.com>
2017-04-10 20:34:41 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,15 @@
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-  set_mtrx <- function(y) {
+  set_mtrx <- function(y) { #set matrix value 
     x <<- y
     inv <<- NULL }
   
-  get_mtrx <- function() x
-  set_inv <- function(inv_mtrx) inv <- inv_mtrx
-  get_inv <- function() inv 
+  get_mtrx <- function() x #get matrix value
+  set_inv <- function(inv_mtrx) inv <- inv_mtrx #set inverse matrix
+  get_inv <- function() inv  #get inverse matrix
   
-  list(setMatrix = set_mtrx, getMatrix = get_mtrx, setInverse = set_inv, getInverse = get_inv)
+  list(setMatrix = set_mtrx, getMatrix = get_mtrx, setInverse = set_inv, getInverse = get_inv) #return a list 
 
 }
 
@@ -22,11 +22,11 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-  inv <- x$get_inv()
+  inv <- x$get_inv() #get the inverse from makeCacheMatrix fumction 
   if(!is.null(inv)) return (inv)
-  data <- x$get_mtrx()
+  data <- x$get_mtrx()  #get the the matrix from makeCacheMatrix fumction 
   inv <- solve(data, ...)
-  x$set_inv(inv)
+  x$set_inv(inv) #get the inverse from makeCacheMatrix fumction 
   inv
   
 }
